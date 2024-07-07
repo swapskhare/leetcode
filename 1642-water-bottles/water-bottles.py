96084f09a9a3e36f62b785def0f9c36f34e1bb33
swapskhare <153451998+swapskhare@users.noreply.github.com>
2024-07-07 10:53:34 -0400

    Time: 45 ms (5.97%) | Memory: 16.4 MB (79.17%) - LeetSync

--- 1642-water-bottles/water-bottles.py
+++ 1642-water-bottles/water-bottles.py
@@ -1,13 +1,12 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-        sumb = numBottles
-        x = numBottles
-        while True:
-            y = x//numExchange
-            z = x%numExchange
-            sumb+=y
-            x = y+z
-            if(x<numExchange):
-                break
-
-        return sumb
+        empty = 0
+        drank = 0
+        while numBottles:
+            drank += 1
+            empty += 1
+            numBottles -= 1
+            if empty == numExchange:
+                empty -= numExchange
+                numBottles += 1
+        return drank
