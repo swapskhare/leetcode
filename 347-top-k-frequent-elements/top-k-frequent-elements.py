17b0d0237784b42b96cae95e08ce192835e8496e
swapskhare <153451998+swapskhare@users.noreply.github.com>
2024-12-23 00:42:36 -0500

    Time: 7 ms (54.53%) | Memory: 24.2 MB (6.48%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,12 +3,35 @@ from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums)
-        heap = []
+        
+        # counts = {}
+        # for num in nums:
+        #     counts[num] = 1 + counts.get(num, 0)
+        
+        # heap = []
+        # for num in counts.keys():
+        #     heapq.heappush(heap,(counts[num],num))
+        #     if len(heap)>k:
+        #         heapq.heappop(heap)
 
-        for key,value in count.items():
-            heapq.heappush(heap, (value,key))
-            if len(heap) > k:
-                heapq.heappop(heap)
+        # res = []
+        # for i in range(k):
+        #     res.append(heapq.heappop(heap)[1])
 
-        return [x[1] for x in heap]
+        # return res
+
+        count = {}
+        freq  = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+        
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+               
