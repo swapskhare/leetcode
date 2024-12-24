c80f41343a53f30c4e06338e98ca58e78298101a
swapskhare <153451998+swapskhare@users.noreply.github.com>
2024-12-24 04:25:44 -0500

    Time: 115 ms (44.72%) | Memory: 18.1 MB (6.98%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,28 +1,38 @@
 class Solution:
-    def mostFreq(self, count):
-        mf = 0
-        for key,value in count.items():
-            mf = max(mf,value)
-
-        return mf
 
     def characterReplacement(self, s: str, k: int) -> int:
-        l, r = 0,0
+        # l, r = 0,0
+        # count = defaultdict(int)
+        # ans = 0
+
+        # while r < len(s):
+        #     count[s[r]] += 1
+        #     mf = max(count[s[r]], )
+        #     wl = r - l + 1
+
+        #     while wl - mf > k:
+        #         count[s[l]] -= 1
+        #         l += 1
+        #         wl -= 1
+            
+        #     ans = max(ans, wl)
+        #     r += 1
+
+        # return ans 
+
+        l,r = 0,0
         count = defaultdict(int)
         ans = 0
-
-        while r < len(s):
+        mf = 0
+        for r in range(len(s)):
             count[s[r]] += 1
-            mf = self.mostFreq(count)
-            wl = r - l + 1
-
-            while wl - mf > k:
+            mf = max(mf, count[s[r]])
+            wlen = r - l + 1
+            while  wlen - mf > k:
                 count[s[l]] -= 1
-                l += 1
-                wl -= 1
-                # mf = self.mostFreq(count)
-            
-            ans = max(ans, wl)
-            r += 1
+                wlen -= 1
+                l+=1
+            ans = max(ans, wlen)
 
-        return ans 
+        return ans
+                
