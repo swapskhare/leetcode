cf563279af0ac3f2f6e7b7faae029f5ce1cf945b
swapskhare <153451998+swapskhare@users.noreply.github.com>
2025-06-02 02:41:25 -0400

    Time: 3 ms (82.16%) | Memory: 17.7 MB (70.94%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,36 +1,44 @@
 from collections import deque
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        directions = [(0,1),(1,0),(-1,0),(0,-1)]
-        rotten = []
-        oranges = 0
+        fresh_count = 0
+        rotten = deque([])
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == 2:
-                    rotten.append((r,c,0))
                 if grid[r][c] == 1:
-                    oranges += 1
+                    fresh_count += 1
+                if grid[r][c] == 2:
+                    rotten.append((r, c))
 
-        queue = deque(rotten)
+        directions = [
+            (0, 1),
+            (1, 0),
+            (0, -1),
+            (-1, 0),
+        ]
         minutes = 0
-        
-        while queue:
-            r, c, level = queue.popleft()
+        while rotten:
+            level_size = len(rotten)
+            minutes += 1
 
-            for dr, dc in directions:
-                nr = r + dr
-                nc = c + dc
-                if (nr >= 0 and nr < ROWS and 
-                    nc >= 0 and nc < COLS and 
-                    grid[nr][nc] == 1):
+            for _ in range(level_size):
+                # process
+                r, c = rotten.popleft()
+                for dr, dc in directions:
+                    nr = r + dr
+                    nc = c + dc
 
-                    queue.append((nr, nc, level + 1))
-                    minutes = max(minutes, level + 1)
-                    grid[nr][nc] = 2
-                    oranges -= 1
-        
-        if oranges > 0:
-            return -1
-        else: return minutes
+                    if (
+                        nr >= 0 and nr < ROWS and
+                        nc >= 0 and nc < COLS and
+                        grid[nr][nc] == 1
+                    ):
+                        rotten.append((nr, nc))
+                        grid[nr][nc] = 2
+                        fresh_count -= 1
+        if minutes:
+            minutes -= 1       
 
+        return -1 if fresh_count > 0 else minutes
